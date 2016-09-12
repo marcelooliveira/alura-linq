791f4eaf9e79f9391db9a6922831bfb42c3e2eb7
marcelooliveira <mclricardo@gmail.com>
2016-09-12 19:07:26 -0300

    .

---
+++ AluraLinq.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alura_linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
