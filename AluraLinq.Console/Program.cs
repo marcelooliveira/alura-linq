d7003784d40dace5f2c856268f9146193aec1044
marcelooliveira <mclricardo@gmail.com>
2016-09-13 12:16:01 -0300

    .

--- AluraLinq.Console/Program.cs
+++ AluraLinq.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace alura_linq
 {
@@ -18,12 +19,26 @@ namespace alura_linq
                 }                
             }
 
+            Console.WriteLine();
+
             using (var context = new AluraTunesEntities())
             {
                 foreach (var cliente in context.Clientes.Take(3))
                 {
                     Console.WriteLine(cliente.Sobrenome);
                 }
+            }
+
+            Console.WriteLine();
+
+            XElement root = XElement.Load(@"C:\Users\caelum\Repos\alura-linq\AluraLinq.Console\Data\Generos.xml");
+
+            var query = from g in root.Descendants("Genero")
+                        select g.Descendants("Nome").First().FirstNode;
+
+            foreach (var genero in query)
+            {
+                Console.WriteLine("{0}", genero);
             }
 
             Console.ReadKey();
