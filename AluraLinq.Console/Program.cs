506ca2656158024cea7b0bc4907cda2043e77eb6
marcelooliveira <mclricardo@gmail.com>
2016-09-27 10:08:14 -0300

    .

--- AluraLinq.Console/Program.cs
+++ AluraLinq.Console/Program.cs
@@ -37,13 +37,13 @@ namespace alura_linq
             //new Problema3().Solve(args);
             //new Problema4().Solve(args);
             //new Problema5().Solve(args);
-            new Problema6().Solve(args);
+            //new Problema6().Solve(args);
             //new Problema7().Solve(args);
             //new Problema8().Solve(args);
             //new Problema9().Solve(args);
             //new Problema10().Solve(args);
             //new Problema11().Solve(args);
-            //new Problema12().Solve(args);
+            new Problema12().Solve(args);
             //new Problema13().Solve(args);
             //new Problema14().Solve(args);
             //new Problema15().Solve(args);
